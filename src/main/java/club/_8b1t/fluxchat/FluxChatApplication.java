b6d41dccafcbf246f63c600e4d2faa63dbbdb8b7
8bit <46142484+wangzhijie0602@users.noreply.github.com>
2026-03-11 18:04:55 +0800

    🎉 init: initial commit

---
+++ src/main/java/club/_8b1t/fluxchat/FluxChatApplication.java
@@ -0,0 +1,13 @@
+package club._8b1t.fluxchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FluxChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/club/_8b1t/fluxchat/FluxChatApplicationTests.java
@@ -0,0 +1,13 @@
+package club._8b1t.fluxchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
